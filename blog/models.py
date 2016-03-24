b358d6960f70bdbce2cbe2ce6921b68d34cd9dae
nick <helloma@yandex.ru>
2016-03-24 12:53:51 +0300

    Add project

---
+++ blog/models.py
@@ -0,0 +1,24 @@
+from __future__ import unicode_literals
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.text import slugify
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length = 100)
+    body = models.TextField()
+    author = models.ForeignKey(User, )
+    added = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now = True)
+    draft = models.BooleanField(default = True)
+    slug = models.SlugField(null = True, blank = True)
+    
+    def __unicode__(self):
+        return self.title
+        
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+        
+    def get_absolute_url(self):
+        return '/blog/%s/' %(self.slug)
---
+++ blog/views.py~
@@ -0,0 +1,33 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post 
+    
+    
+from django.shortcuts import redirect
+# Create your views here.
+
+def home(request):
+    posts = Post.objects.filter(draft=False)
+    context = {
+        'posts': posts,
+    
+    }
+    return render(request, 'blog/home.html', context)
+    
+
+
+def redirect_to_blog(request):
+    return redirect('/blog')
+    
+    
+def detail(request, slug=None):
+
+    post = get_object_or_404(Post, slug=slug)
+    
+    
+    context = {
+        'post': post,
+    
+    }
+    
+    
+    return render(request, 'blog/detail.html', context)
